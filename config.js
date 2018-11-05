6cbaf7bc981a706bd037cfea96ba1a3dde32d5cd
kyle_koh <ykoh728@gmail.com>
2018-11-05 18:16:44 +0900

    add token for authentication

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
     PORT: process.env.PORT || 3000,
     URL: process.env.BASE_URL || 'http://localhost:3000',
     MONGODEB_URI: process.env.MONGODEB_URI ||
-    'mongodb://test123:test123@ds151943.mlab.com:51943/restify_customer'
+    'mongodb://test123:test123@ds151943.mlab.com:51943/restify_customer',
+    JWT_SECRET: process.env.JWT_SECRET || 'secret1'
 }
